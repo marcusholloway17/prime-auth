b67e33848e3e7cbdab3b9bf04ebc2fb93ec40238
marcusholloway17 <loganjean17@gmail.com>
2024-11-10 20:22:26 +0000

    scope service initialized

--- services/scope.service.ts
+++ services/scope.service.ts
@@ -1,9 +1,23 @@
-import { Injectable } from '@angular/core';
+import { Inject, Injectable } from '@angular/core';
+import { CrudService } from '../../../helpers/crud/crud.service';
+import { HttpClient } from '@angular/common/http';
+import { AUTH_CONFIG_PROVIDER, AuthConfigType, ScopeType } from '../types';
 
 @Injectable({
   providedIn: 'root'
 })
-export class ScopeService {
+export class ScopeService extends CrudService<ScopeType> {
+  public override identifier: string = 'id';
 
-  constructor() { }
+  constructor(httpClient: HttpClient, @Inject(AUTH_CONFIG_PROVIDER) private authConfig: AuthConfigType) {
+    super(httpClient);
+    this.set_url(`${authConfig.apiHost}/api/scopes/`);
+    this.setQueryParams({
+      _query: {
+        include: ["Roles"],
+      },
+      page: 1,
+      pageSize: 50
+    })
+  }
 }
