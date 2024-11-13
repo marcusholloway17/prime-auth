6bc0ecb82403ead89ed35d8cd8a670172f3f8847
marcusholloway17 <loganjean17@gmail.com>
2024-11-13 06:49:32 +0000

    scope views component declared

--- auth.module.ts
+++ auth.module.ts
@@ -34,9 +34,13 @@ import { MultiSelectModule } from 'primeng/multiselect';
 import { MessageModule } from 'primeng/message';
 import { ChipModule } from 'primeng/chip';
 import { CheckboxModule } from 'primeng/checkbox';
+import { ScopeListComponent } from './pages/scopes/scope-list/scope-list.component';
+import { ScopeCreateComponent } from './pages/scopes/scope-create/scope-create.component';
+import { ScopeDetailComponent } from './pages/scopes/scope-detail/scope-detail.component';
+import { ScopeEditComponent } from './pages/scopes/scope-edit/scope-edit.component';
 
 @NgModule({
-  declarations: [CallbackComponent, SignOutComponent, UserCreateComponent, UserListComponent, UserEditComponent, UserDetailComponent, LayoutComponent, RoleListComponent, RoleCreateComponent, RoleDetailComponent, RoleEditComponent],
+  declarations: [CallbackComponent, SignOutComponent, UserCreateComponent, UserListComponent, UserEditComponent, UserDetailComponent, LayoutComponent, RoleListComponent, RoleCreateComponent, RoleDetailComponent, RoleEditComponent, ScopeListComponent, ScopeCreateComponent, ScopeDetailComponent, ScopeEditComponent],
   imports: [CommonModule, FormsModule, AuthRoutingModule, ProgressSpinnerModule, ButtonModule, TranslateModule, SplitButtonModule, InputTextareaModule, DividerModule, CardModule, TableModule, InputTextModule, IconFieldModule, InputIconModule, SelectModule, TagModule, PaginatorModule, ToggleSwitchModule, MultiSelectModule, MessageModule, ChipModule, CheckboxModule],
 })
 export class AuthModule {
