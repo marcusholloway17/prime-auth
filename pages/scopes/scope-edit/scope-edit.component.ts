a6bd94451b6d500237f9cf8529692636b02c5f06
marcusholloway17 <loganjean17@gmail.com>
2024-11-13 07:24:00 +0000

    scope-edit view initialized

---
+++ pages/scopes/scope-edit/scope-edit.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-scope-edit',
+  templateUrl: './scope-edit.component.html',
+  styleUrl: './scope-edit.component.css'
+})
+export class ScopeEditComponent {
+
+}
