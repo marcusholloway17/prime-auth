31c106324313ad5d1f67e3650f3a471d3d842949
marcusholloway17 <loganjean17@gmail.com>
2024-11-14 15:54:54 +0000

    setup role edit view

--- pages/roles/role-edit/role-edit.component.ts
+++ pages/roles/role-edit/role-edit.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
-import { catchError, forkJoin, Subject, take, takeUntil, tap, throwError } from 'rxjs';
-import { RoleType } from '../../../types';
+import { Component, ViewChild } from '@angular/core';
+import { catchError, filter, forkJoin, of, Subject, switchMap, take, takeUntil, tap, throwError } from 'rxjs';
+import { RoleScopeType, RoleType, ScopeType } from '../../../types';
 import { ScopeService } from '../../../services/scope.service';
 import { RoleService } from '../../../services/role.service';
 import { ConfirmationService } from 'primeng/api';
 import { RoleScopeService } from '../../../services/role-scope.service';
+import { Popover } from 'primeng/popover';
 
 @Component({
   selector: 'app-role-edit',
@@ -14,7 +15,12 @@ import { RoleScopeService } from '../../../services/role-scope.service';
 export class RoleEditComponent {
   private destroy$ = new Subject<void>();
   public role: Partial<RoleType> = {};
+  public available_scopes: ScopeType[] = [];
+  public selected_scopes: ScopeType[] = [];
   public success_alert: boolean = false;
+
+  // popover
+  @ViewChild('op', { static: false }) op!: Popover;
 
 
   constructor(
@@ -33,10 +39,10 @@ export class RoleEditComponent {
   }
 
   load_select_data() {
-    forkJoin(
+    return forkJoin(
       [
         this.roleService$.active_data$.pipe(tap((state) => { if (state && state.id) { this.role = state } })),
-        this.scopeService$.list({ page: undefined, pageSize: undefined, _query: {} }).pipe(takeUntil(this.destroy$), take(1))
+        this.scopeService$.list<ScopeType>({ page: undefined, pageSize: undefined, _query: { order: [["createdAt", "DESC"]] } }).pipe(takeUntil(this.destroy$), take(1))
       ]
     )
       .subscribe();
@@ -76,7 +82,40 @@ export class RoleEditComponent {
     })
   }
 
-  on_scope_select_change(event: any) {
-    console.log('scope select change', event);
+  on_scope_removed(scope: ScopeType) {
+    if (scope && scope.RoleScope) {
+      this.roleScopeService$.delete(scope.RoleScope.id).pipe(
+        takeUntil(this.destroy$),
+        take(1),
+        switchMap(() => of(this.load_select_data()))
+      ).subscribe();
+    }
+  }
+  on_scope_added() {
+    if (this.selected_scopes?.length)
+      forkJoin(this.selected_scopes.map(
+        scope => this.roleService$.active_data$.pipe(
+          takeUntil(this.destroy$),
+          take(1),
+          filter((role) => role != null),
+          switchMap((role) => this.roleScopeService$.create<RoleScopeType>({ roleId: role.id, scopeId: scope.id }).pipe(
+            takeUntil(this.destroy$),
+            take(1),
+          ))
+        ))).pipe(
+          takeUntil(this.destroy$),
+          switchMap(() => this.roleService$.refresh_active_data().pipe(take(1))),
+          tap(() => this.selected_scopes = [])
+        )
+        .subscribe();
+  }
+
+  toggle_popover(event: Event, selected_scopes: ScopeType[]) {
+    this.scopeService$.data$.pipe(
+      tap((state) => {
+        this.available_scopes = state.data.filter(scope => !selected_scopes.find(e => e.id == scope.id));
+        this.op.toggle(event);
+      })
+    ).subscribe();
   }
 }
